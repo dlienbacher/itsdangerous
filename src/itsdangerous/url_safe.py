952d80f860e7f8bd399b4c4de4d0a7cb40174936
dlienbacher <43840977+dlienbacher@users.noreply.github.com>
2024-04-03 15:07:57 +0200

    Update url_safe.py

--- src/itsdangerous/url_safe.py
+++ src/itsdangerous/url_safe.py
@@ -51,18 +51,7 @@ class URLSafeSerializerMixin(Serializer):
 
     def dump_payload(self, obj: _t.Any) -> bytes:
         json = super().dump_payload(obj)
-        is_compressed = False
-        compressed = zlib.compress(json)
-
-        if len(compressed) < (len(json) - 1):
-            json = compressed
-            is_compressed = True
-
         base64d = base64_encode(json)
-
-        if is_compressed:
-            base64d = b"." + base64d
-
         return base64d
 
 
